ab99c58f40c53032aa377adb3dd1f11e1b590e8b
Prabin Pun <prabin1punah@gmail.com>
2018-10-22 14:42:52 -0400

    Added Navigation

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -15,31 +15,31 @@ namespace SportsStore.UnitTests
     [TestClass]
     public class UnitTest1
     {
-        //[TestMethod]
-        //public void Can_Paginate()
-        //{
-        //    /*
-        //    Mock<IProductRepository> mock = new Mock<IProductRepository>();
-        //    mock.Setup(m => m.Products).Returns(new Product[] {
-        //        new Product {ProductID = 1, Name = "P1"},
-        //        new Product {ProductID = 2, Name = "P2"},
-        //        new Product {ProductID = 3, Name = "P3"},
-        //        new Product {ProductID = 4, Name = "P4"},
-        //        new Product {ProductID = 5, Name = "P5"}
-        //    });
-        //    ProductController controller = new ProductController(mock.Object);
-        //    controller.PageSize = 3;
-        //    // Act
+        [TestMethod]
+        public void Can_Paginate()
+        {
+            
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product {ProductID = 1, Name = "P1"},
+                new Product {ProductID = 2, Name = "P2"},
+                new Product {ProductID = 3, Name = "P3"},
+                new Product {ProductID = 4, Name = "P4"},
+                new Product {ProductID = 5, Name = "P5"}
+            });
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+            // Act
 
-        //    IEnumerable<Product> result =
-        //    (IEnumerable<Product>)controller.List(2).Model;
-        //    // Assert
-        //    Product[] prodArray = result.Products.ToArray();
-        //    Assert.IsTrue(prodArray.Length == 2);
-        //    Assert.AreEqual(prodArray[0].Name, "P4");
-        //    Assert.AreEqual(prodArray[1].Name, "P5");
-        //    */
-        //}
+            ProductsListViewModel result =
+            (ProductsListViewModel)controller.List(null, 2).Model;
+            // Assert
+            Product[] prodArray = result.Products.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
+            
+        }
         [TestMethod]
         public void Can_Generate_Page_Links()
         {
@@ -59,9 +59,9 @@ namespace SportsStore.UnitTests
             MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageUrlDelegate);
             //Asset
             Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>"
-        + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
+             + @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>"
                 + @"<a class=""btn btn-default"" href=""Page3"">3</a>",
-result.ToString());
+            result.ToString());
         }
 
         [TestMethod]
@@ -80,7 +80,7 @@ result.ToString());
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
             // Act
-            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(null, 2).Model;
             // Assert
             PagingInfo pageInfo = result.PagingInfo;
             Assert.AreEqual(pageInfo.CurrentPage, 2);
